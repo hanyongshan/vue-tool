94d9b9231fd1425b6b8308a1273fab6c520ff446
hanyongshan <1752378724@qq.com>
2025-05-21 23:04:47 +0800

    添加字体图标

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-import Router from "./router/Index";
+import Vue from 'vue';
+import App from './App.vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Router from './router/Index';
+import '@/assets/font/iconfont.css'; //从阿里云引入字体图标
+import ComponentUtil from '../package/index.js';
 
-import ComponentUtil from "../package/index.js";
-
-import "../package/assets/css/base.less";
+import '../package/assets/css/base.less';
 
 Vue.config.productionTip = false;
 
@@ -14,6 +14,6 @@ Vue.use(ElementUI);
 Vue.use(ComponentUtil);
 
 new Vue({
-  render: (h) => h(App),
+  render: h => h(App),
   router: Router,
-}).$mount("#app");
+}).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,15 @@
     <div @click="go('/datepick')">日期</div>
     <div>按钮</div>
     <div>按钮</div>
+    <i class="el-icon-edit"></i>
+    <i class="icon_td-disable-F"></i>
   </div>
 </template>
 
 <script>
 export default {
   // import引入的组件需要注入到对象中才能使用
-  name: "HomeIndex",
+  name: 'HomeIndex',
   components: {},
   data() {
     // 这里存放数据
